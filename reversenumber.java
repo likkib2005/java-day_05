e1e5fd110c22fa0b5f82035c913c44bfd520c7ab
likkib2005 <likkilikitha214@gmail.com>
2024-12-06 10:43:51 +0530

    Add files via upload

---
+++ reversenumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class reversenumber
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    for(int i=0;i<t;i++)
+    {
+    int n=sc.nextInt();
+    int rev=0;
+    while(n>0)
+    {
+        int digit=n%10;
+        rev=rev*10+digit;
+        n/=10;
+    }
+    System.out.println(rev);   
+    }
+    }
+}
