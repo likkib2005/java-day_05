b758dd5aed5c4b204520702497e2b03c8bbdc4eb
likkib2005 <likkilikitha214@gmail.com>
2024-12-06 10:53:54 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class armstrong
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    for(int i=0;i<t;i++)
+    {
+    int n=sc.nextInt();
+    int temp=n;
+    int rev=0;
+    while(n>0)
+    {
+        int digit=n%10;
+        rev=rev+digit*digit*digit;
+        n/=10;
+    }
+    System.out.println(rev);
+    if(temp==rev)
+    System.out.println("armstrong");
+    else
+    System.out.println("not armstrong");  
+    }
+    }
+}
