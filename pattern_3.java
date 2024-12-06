4f50daf7469564265f393f65c1f8f4e76fb5ffac
likkib2005 <likkilikitha214@gmail.com>
2024-12-06 13:32:47 +0530

    Add files via upload

---
+++ pattern_3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pattern_3
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<=5;i++)
+        {
+            for( int j=0;j<=5;j++)
+            {
+            System.out.print("*");    
+            }
+            System.out.println();
+        }
+    }
+}
