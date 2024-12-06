988284aa681dc838c02711675b0172f1c46b33cb
likkib2005 <likkilikitha214@gmail.com>
2024-12-06 12:37:24 +0530

    Add files via upload

---
+++ pattern_1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pattern_1
+{
+    public static void main(String args[])
+    {
+        for(int i=0;i<=5;i++)
+        {
+            for( int j=0;j<=5;j++)
+            {
+            System.out.println(j+" ");    
+            }
+            System.out.println();
+        }
+    }
+}
